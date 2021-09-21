0cfbce39b53950b19008ad3ab2bd63b2ca671f61
appkenny <kzeng.95@gmail.com>
2021-09-20 20:42:40 -0700

    useEffect to fetch from api on mount

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,20 +2,29 @@ import React, {useState, useEffect} from 'react';
 import RickandMortyAPI from '../util/r&mAPI'
 
 function Main() {
-  function setData(res) {
-    setState(res)
-  }
+  // function setData(res) {
+  //   setState(res)
+  // }
+  
+  // const [state, setState] = useState( () => {
+    //   RickandMortyAPI.fetchData().then(res => {
+      //     setData(res)
+      //   })
+  // })
+      
+  const [state, setState] = useState(null)
 
-  const [state, setState] = useState( () => {
+  useEffect( () => {
+    console.log("fetching")
     RickandMortyAPI.fetchData().then(res => {
-      setData(res)
+      setState(res)
     })
-  })
+  }, [])
 
   return(
     <div className="main-container">
       {console.log(state)}
-      <button onClick={ () => {console.log(state)}} />
+      <button onClick={ () => {console.log(state.results)}} />
     </div>
   )
 }
